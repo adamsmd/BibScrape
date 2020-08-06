12ffbc4c917f142479d5ecfa4e119821fe7e8dc9
Michael D. Adams <adamsmd@users.noreply.github.com>
2020-08-06 11:54:42 -0400

    Make test.sh detect where bibscrape is

--- test.sh
+++ test.sh
@@ -13,6 +13,18 @@
 #
 #     $ ./test.sh --show-window tests/acm-*.t
 
+# Determine where `bibscrape` is based on the location of this script
+if [ -z "$BIBSCRAPE" ]; then
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  BIBSCRAPE="$DIR"/bin/bibscrape
+fi
+
 GLOBAL_FLAGS=()
 
 while test $# -gt 0; do
@@ -34,12 +46,12 @@ for i in "$@"; do
   echo "** [$(date +%r)] Testing $i using a URL **"
   URL=\"$(head -n 1 "$i")\"
   FLAGS=$(head -n 2 "$i" | tail -1)
-  if !(head -n 3 "$i"; eval timeout 60s ./bin/bibscrape $FLAGS "${GLOBAL_FLAGS[@]}" "$URL" 2>&1) | diff -u "$i" - | wdiff -dt; then
+  if !(head -n 3 "$i"; eval timeout 60s "$BIBSCRAPE" $FLAGS "${GLOBAL_FLAGS[@]}" "$URL" 2>&1) | diff -u "$i" - | wdiff -dt; then
     true $((ERR_COUNT++))
   fi
 
   echo "** [$(date +%r)] Testing $i using a filename **"
-  if ! eval timeout 60s ./bin/bibscrape $FLAGS "${GLOBAL_FLAGS[@]}" <(grep -v '^WARNING: Suspect name: ' "$i") 2>&1 | diff -u "$i" - | wdiff -dt; then
+  if ! eval timeout 60s "$BIBSCRAPE" $FLAGS "${GLOBAL_FLAGS[@]}" <(grep -v '^WARNING: ' "$i") 2>&1 | diff -u "$i" - | wdiff -dt; then
     true $((ERR_COUNT++))
   fi
 done
